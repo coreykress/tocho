48a395b43aa425d5b059d44e76e8fc6ce9c9487e
Corey Kress <coreykre@buffalo.edu>
2017-10-01 11:46:27 -0400

    attempt to add credit card

--- green-napkin/src/DeliveryForm.js
+++ green-napkin/src/DeliveryForm.js
@@ -5,6 +5,11 @@ class DeliveryForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            number: 0,
+            name: "",
+            expiry: "",
+            cvc: "",
+            focused: "false",
 
         };
     }
@@ -13,6 +18,7 @@ class DeliveryForm extends Component {
         return (
             <div className="delivery-form-container">
                 <h3 className="main-text">Delivery</h3>
+
             </div>
         );
     }
